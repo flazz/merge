84ef6a053f49318e99b70eafd33fe190fff96c6c
Francesco Lazzarino <flazzarino@gmail.com>
2014-07-09 15:03:21 -0500

    sorts buffers

--- merge.c
+++ merge.c
@@ -3,7 +3,7 @@
 #include <sys/select.h>
 #include <unistd.h>
 #include <errno.h>
-
+#include <string.h>
 #include <fcntl.h>
 
 /*
@@ -31,9 +31,13 @@ typedef unsigned long BITSET;
 #define BS_TEST(set, n) set & (1 << n)
 
 /* machine state */
-char buffers[MAX_INPUTS][MAX_RECORD_WITDH];
-int buffer_size[MAX_INPUTS];
 BITSET enabled_input_fds;
+char in_buffer[MAX_INPUTS][MAX_RECORD_WITDH];
+int in_buffer_size[MAX_INPUTS];
+
+int out_buffer_count = 0;
+char out_buffer[MAX_INPUTS][MAX_RECORD_WITDH];
+int out_buffer_size[MAX_INPUTS];
 
 /* global select context */
 int fdns;
@@ -61,13 +65,13 @@ prepare_input()
   return 0;
 }
 
-int
+void
 read_data()
 {
   for(int fd=0; fd<MAX_INPUTS; fd++) {
     if (BS_TEST(enabled_input_fds, fd) && FD_ISSET(fd, &inputs)) {
-      int len = read(fd, buffers[fd], MAX_RECORD_WITDH);
-      buffer_size[fd] = len;
+      int len = read(fd, in_buffer[fd], MAX_RECORD_WITDH);
+      in_buffer_size[fd] = len;
 
       switch (len) {
         case 0:
@@ -77,16 +81,45 @@ read_data()
           break;
 
         default:
-          DEBUG("fd %d: %d - %s", fd, buffer_size[fd], buffers[fd]);
+          DEBUG("fd %d: %d - %s", fd, in_buffer_size[fd], in_buffer[fd]);
           break;
       }
 
     }
   }
-  return 0;
 }
 
 int
+buffercmp(const void *s1, const void *s2)
+{
+  return memcmp(s1, s2, MAX_RECORD_WITDH);
+}
+
+void
+transfer_buffers()
+{
+  for(int fd=0; fd<MAX_INPUTS; fd++) {
+    if (BS_TEST(enabled_input_fds, fd) && in_buffer_size[fd]) {
+      memset(out_buffer[out_buffer_count], 0, MAX_RECORD_WITDH);
+      memcpy(out_buffer[out_buffer_count], in_buffer[fd], in_buffer_size[fd]);
+      out_buffer_size[out_buffer_count] = in_buffer_size[fd];
+
+      in_buffer_size[fd] = 0;
+      out_buffer_count++;
+    }
+  }
+  qsort(out_buffer, out_buffer_count, MAX_RECORD_WITDH, &buffercmp);
+}
+
+void
+write_data()
+{
+  for(int i=0; i < out_buffer_count; i++) {
+    write(1, out_buffer[i], out_buffer_size[i]);
+  }
+}
+
+void
 show_select_error()
 {
 
@@ -128,8 +161,6 @@ show_select_error()
       DEBUG("crap out");
       break;
   }
-
-  return 0;
 }
 
 int
@@ -153,10 +184,14 @@ main(int argc, char* argv[])
 
     if (rs < 0) {
       show_select_error();
+      exit(1);
     } else if (rs == 0) {
       DEBUG("should never happen");
+      exit(2);
     } else {
       read_data();
+      transfer_buffers();
+      write_data();
     }
 
   }
